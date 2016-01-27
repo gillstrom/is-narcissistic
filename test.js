8495ecb5822fa0a4fc898e45aae15b340bff2e88
gillstrom <andreasgillstrom@gmail.com>
2016-01-27 16:41:14 +0100

    Adjust tests according to new AVA api

--- test.js
+++ test.js
@@ -1,21 +1,19 @@
 import test from 'ava';
-import fn from './';
+import m from './';
 
 test('should be true', t => {
-	t.true(fn(153));
-	t.true(fn(370));
-	t.true(fn(371));
-	t.true(fn(407));
-	t.true(fn(1634));
-	t.true(fn(8208));
-	t.true(fn(4679307774));
-	t.end();
+	t.true(m(153));
+	t.true(m(370));
+	t.true(m(371));
+	t.true(m(407));
+	t.true(m(1634));
+	t.true(m(8208));
+	t.true(m(4679307774));
 });
 
 test('should be false', t => {
-	t.false(fn(154));
-	t.false(fn(471414));
-	t.false(fn(88));
-	t.false(fn(100));
-	t.end();
+	t.false(m(154));
+	t.false(m(471414));
+	t.false(m(88));
+	t.false(m(100));
 });
